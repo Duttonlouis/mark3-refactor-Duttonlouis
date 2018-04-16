4624379a9042f4fb3339111adc4247a74fa51ba5
LouisDutton <Duttonlouis@gmail.com>
2018-04-16 08:58:51 -0500

    change name class

--- RefactorKata/Program.cs
+++ RefactorKata/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace RefactorKata
 {
@@ -7,35 +8,26 @@ namespace RefactorKata
     {
         static void Main(string[] args)
         {
-            //This is intentionally bad : (  Let's Refactor!
-            var conn = new System.Data.SqlClient.SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
 
-            System.Data.SqlClient.SqlCommand cmd = conn.CreateCommand();
+            var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
+
+            var cmd = conn.CreateCommand();
             cmd.CommandText = "select * from Products";
-            /*
-             * cmd.CommandText = "Select * from Invoices";
-             */
-            System.Data.SqlClient.SqlDataReader reader = cmd.ExecuteReader();
-            List<Product> products = new List<Product>();
 
-            //TODO: Replace with Dapper
+            var reader = cmd.ExecuteReader();
+            var products = new List<Product>();
+
             while (reader.Read())
             {
-                var prod = new Product();
-                prod.name = reader["Name"].ToString();
-                products.Add(prod);
+                products.Add(new Product { Name = reader["Name"].ToString() });
             }
             conn.Dispose();
             Console.WriteLine("Products Loaded!");
-            for (int i =0; i< products.Count; i++)
+
+            for (int i = 0; i < products.Count; i++)
             {
-                Console.WriteLine(products[i].name);
+                Console.WriteLine(products[i].Name);
             }
         }
-    }
-    public class Product
-    {
-        public string name;
-        public string Name { get { return name; } set { name = value; } }
     }
 }
